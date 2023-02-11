2349b07ad910934d0a83817a8109518cbfacfca6
Fombi Favour <fombifavour@gmail.com>
2023-02-11 14:27:31 +0100

    initializing dashboard

---
+++ src/components/DashNavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { IoAdd, IoMdSearch, IoIosLogOut } from 'react-icons/io'
+
+import { auth } from '../firebase-config'
+import { signout } from 'firebase/auth'
+
+const DashNavbar = () => {
+  return (
+    <div>DashNavbar</div>
+  )
+}
+
+export default DashNavbar
